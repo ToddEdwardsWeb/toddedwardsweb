d56fb32e068b53f399b9ae21848569cb0241e82f
Todd S. Edwards <todde@toddedwardsweb.com>
2019-01-06 10:58:55 -0500

    First commit. Sync'd Splash page in PROD. Projects, Articles in DEV.

---
+++ README.md
@@ -0,0 +1,8 @@
+# This master will be repositry for ToddEdwardsWeb.com code.
+
+## This repo includes a devops environment with Node.js packages using Grunt pre-processing SASS/SCSS and HTML markup.
+
+###### ToddEdwardsWeb is coded using the Bootstrap 4.x framework. (List package updates)
+###### Initial commit 11.28.2018.
+
+
---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        express : {
+            dev: {
+                options: {
+                    compress: true,
+                    script: 'app.js'
+      }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'site_root/css/bootstrap_te.css' : 'site_root/scss/bootstrap/bootstrap_te.scss',
+                    'site_root/css/theme-te45.css' : 'site_root/scss/theme-te45.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.registerTask('dev',['express','sass','watch']);
+}
